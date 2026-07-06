ee3a3c0b6375db93ba6ad2191386f3c9d5f5fae3
Matt Didon <matt@onlx.co.uk>
2026-07-06 21:01:04 +0100

    Update for imports
    #79 Changes to imports to support commonjs and ESM, default and named

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import BonjourService, { Service, Browser }                     from './lib/bonjour'
-import type { ServiceReferer, ServiceConfig, BrowserConfig }    from './lib/bonjour'
+import BonjourService, { Service as ServiceClass, Browser as BrowserClass }                                                     from './lib/bonjour'
+import type { ServiceReferer as ServiceRefererType, ServiceConfig as ServiceConfigType, BrowserConfig as BrowserConfigType }    from './lib/bonjour'
 
 class Bonjour extends BonjourService {}
 
-export { Bonjour, Service, Browser }
-export type { ServiceReferer, ServiceConfig, BrowserConfig }
-export default Bonjour
+const BonjourConstructor = Bonjour
 
-module.exports = Object.assign(Bonjour, {
-    Bonjour,
-    Service,
-    Browser,
-    default: Bonjour
+namespace Bonjour {
+    export const Bonjour = BonjourConstructor
+    export const Service = ServiceClass
+    export const Browser = BrowserClass
+    export type ServiceReferer = ServiceRefererType
+    export type ServiceConfig = ServiceConfigType
+    export type BrowserConfig = BrowserConfigType
+}
+
+Object.defineProperty(Bonjour, 'default', {
+    enumerable: true,
+    value: Bonjour,
 })
+
+module.exports.Bonjour = Bonjour
+module.exports.Service = ServiceClass
+module.exports.Browser = BrowserClass
+module.exports.default = Bonjour
+
+export = Bonjour
